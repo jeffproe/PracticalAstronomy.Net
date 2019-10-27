760a3266cb85145409dba5da666501b538b26001
Jeff Proefrock <jeffproe@gmail.com>
2019-10-26 22:55:59 -0400

    Calculate Easter Sunday (#1)

---
+++ src/PANet/Dates.cs
@@ -0,0 +1,26 @@
+﻿namespace System
+{
+	public static class Dates
+	{
+		public static DateTime EasterSunday(this DateTime theDate)
+		{
+			int a = theDate.Year % 19;
+			int b = theDate.Year / 100;
+			int c = theDate.Year % 100;
+			int d = b / 4;
+			int e = b % 4;
+			int f = (b + 8) / 25;
+			int g = (b - f + 1) / 3;
+			int h = ((19 * a) + b - d - g + 15) % 30;
+			int i = c / 4;
+			int k = c % 4;
+			int l = (32 + (2 * e) + (2 * i) - h - k) % 7;
+			int m = (a + (11 * h) + (22 * l)) / 451;
+			int n = (h + l - (7 * m) + 114) / 31;
+			int p = (h + l - (7 * m) + 114) % 31;
+
+			var easter = new DateTime(theDate.Year, n, p + 1);
+			return easter;
+		}
+	}
+}
---
+++ src/runner/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace runner
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool run = true;
+			while (run)
+			{
+				Console.WriteLine("PANet\n");
+
+				Console.WriteLine("1) Easter Sunday");
+				Console.WriteLine("Q) Quit");
+
+				Console.Write("\nSelection: ");
+
+				var option = Console.ReadLine();
+
+				if (option.Equals("q", StringComparison.OrdinalIgnoreCase))
+				{
+					run = false;
+					continue;
+				}
+
+				int opt = 0;
+				if (int.TryParse(option, out opt))
+				{
+					switch (opt)
+					{
+						case 1:
+							EasterSunday();
+							break;
+					}
+				}
+
+				Console.WriteLine("==========\n");
+			}
+		}
+
+		static void EasterSunday()
+		{
+			try
+			{
+				Console.Write($"Enter year to get Easter Sunday for [{DateTime.Now.Year}]: ");
+				var year = Console.ReadLine();
+
+				bool validYear = true;
+				int y = DateTime.Now.Year;
+				if (!string.IsNullOrWhiteSpace(year))
+				{
+					if (!int.TryParse(year, out y))
+					{
+						validYear = false;
+						Console.WriteLine("Invalid year");
+					}
+				}
+				if (validYear)
+				{
+					Console.WriteLine($"\tEaster Sunday is {new DateTime(y, DateTime.Now.Month, DateTime.Now.Day).EasterSunday().ToShortDateString()}");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error calculating Easter Sunday: {0}", ex);
+			}
+		}
+	}
+}
